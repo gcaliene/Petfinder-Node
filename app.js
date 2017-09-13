17b52229113a2336518d6c6eaf98e485feb5efbf
gerson C <gersoncalienes@gmail.com>
2017-09-13 14:42:58 -0400

    added app.js with mock data

---
+++ app.js
@@ -0,0 +1,34 @@
+//where mock data will go.
+
+var MOCK_PET_SIGHTINGS = {
+	"sightings" : [
+		{
+			"id": 11111,
+			"text": "Lost puppy seen behind the gas station",
+			"userId": "aaaaa",
+			"userName": "mary joe",
+			"publishedAt": 999999
+		},
+		{
+			"id": 22222,
+			"text": "I think I saw a cat rummaging my trash can",
+			"userId": "bbbbb",
+			"userName": "jon smith",
+			"publishedAt": 888888
+		},
+		{
+			"id": 33333,
+			"text": "Lost puppy wandering the park by the school",
+			"userId": "ccccc",
+			"userName": "corey martin",
+			"publishedAt": 777777
+		},
+		{
+			"id": 44444,
+			"text": "kittens heard around the community pool around noon",
+			"userId": "ddddd",
+			"userName": "dylan doug",
+			"publishedAt": 666666
+		},
+	]
+};
