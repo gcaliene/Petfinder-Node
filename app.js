1b3419a19c233525378f926fa4f537f0f598d685
gerson C <gersoncalienes@gmail.com>
2017-09-13 16:57:35 -0400

    added js src in html and added basic functionality to mock data

--- app.js
+++ app.js
@@ -32,3 +32,35 @@ var MOCK_PET_SIGHTINGS = {
 		},
 	]
 };
+
+// this function's name and argument can stay the
+// same after we have a live API, but its internal
+// implementation will change. Instead of using a
+// timeout function that returns mock data, it will
+// use jQuery's AJAX functionality to make a call
+// to the server and then run the callbackFn
+function getRecentStatusUpdates(callbackFn) {
+    // we use a `setTimeout` to make this asynchronous
+    // as it would be with a real AJAX call.
+	setTimeout(function(){ callbackFn(MOCK_PET_SIGHTINGS)}, 1);
+}
+
+// this function stays the same when we connect
+// to real API later
+function displayStatusUpdates(data) {
+    for (index in data.statusUpdates) {
+	   $('body').append(
+        '<p>' + data.statusUpdates[index].text + '</p>');
+    }
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayStatusUpdates() {
+	getRecentStatusUpdates(displayStatusUpdates);
+}
+
+//  on page load do this
+$(function() {
+	getAndDisplayStatusUpdates();
+})
