d9cee6dbaab198154f3767741b7d9c8667d7b008
gerson C <gersoncalienes@gmail.com>
2017-09-21 11:06:18 -0400

    added a mocha test

--- public/app.js
+++ public/app.js
@@ -1,9 +1,19 @@
 //this is where the front end javascript code goes
 $("#form-js").submit(function(event){
 	event.preventDefault();
-	
+	//what happens when submit is selected
+	console.log("you just clicked submit");
 });
 
+function getValues (){
+	var description = $("#description").val();
+	var name = $("#name").val();
+
+	$.ajax({
+		type:'POST'
+		ur
+	})
+}
 
 
 
