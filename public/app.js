2ac13136c2b4f4334bc3dab77cfcfe68d07cdc0b
gerson C <gersoncalienes@gmail.com>
2017-09-27 18:23:41 -0400

    added buttons in index.js

--- public/app.js
+++ public/app.js
@@ -33,22 +33,23 @@ var $name = $("#name");
 			url:'/posts',
 			data: post,
 			success: function(newPost) {
-				$posts.append('<li> <input type="button" id="deleteButton"> Delete</input> <input type="button" id="updateButton">Update</input> text: ' + post.text + " name: " + post.userName + '  at ' + Date(post.created) + ' </li>');
+				$posts.append('<li> <button type="button" id="deleteButton"> Delete</button> <button type="button" id="updateButton">Update</button> text: ' + post.text + " name: " + post.userName + '  at ' + Date(post.created) + ' </li>');
 			},
 			error: function(){
 				alert('Couldn\'t load previous posts!');
 			},
 		})
 	});
-});
 
+	
+	$posts.delegate('#removeButton','click', function(){
+		console.log('Sending request to Ajax to delete post'); //not showing up because no delete button exists yet.
 
-$(function(){
-	$("#deleteButton").on('click', function(){
-		console.log('Sending request to Ajax to delete post'); //not showing up
 
 	});
 });
+
+
 
 
 
