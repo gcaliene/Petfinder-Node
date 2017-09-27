f9e10f0c7d9195bd4684096e64ab1806428c0fce
gerson C <gersoncalienes@gmail.com>
2017-09-27 17:37:59 -0400

    added buttons in index.js

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ var $name = $("#name");
 		url: '/posts',
 		success: function(posts){
 			$.each(posts, function(index, post){
-				$posts.append('<li> <button id="deleteButton"> <b>text:</b> ' + post.text + " name: " + post.name + ' at ' + Date(post.created) + '</li>');
+				$posts.append('<li> <button id="deleteButton"> Delete</button> <button id="updateButton">Update</button> <b>text:</b> ' + post.text + " name: " + post.name + ' at ' + Date(post.created) + '</li>');
 			});
 		},
 		error: function(){
@@ -23,27 +23,25 @@ var $name = $("#name");
 		event.preventDefault();
 		//what happens when submit is selected
 		console.log("you just clicked submit");
-
-
-		
 		var post = {
 			text: $text.val(),
 			userName: $name.val(),
 			created: new Date(),
 		};
-
 		$.ajax({
 			type:'POST',
 			url:'/posts',
 			data: post,
 			success: function(newPost) {
-				$posts.append('<li> text: ' + post.text + " name: " + post.userName + '  at ' + post.created + ' </li>');
+				$posts.append('<li> <button id="deleteButton"> Delete</button> <button id="updateButton">Update</button> text: ' + post.text + " name: " + post.userName + '  at ' + post.created + ' </li>');
 			},
 			error: function(){
 				alert('Couldn\'t load previous posts!');
 			},
 		})
 	});
+
+
 });
 
 
