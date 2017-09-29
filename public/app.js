c0c06063c402e553eb6eaa486e2bf78e57f70587
gerson C <gersoncalienes@gmail.com>
2017-09-29 10:16:36 -0400

    first commit in new branch

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ var $name = $("#name");
 		url: '/posts',
 		success: function(posts){
 			$.each(posts, function(index, post){
-				$posts.append('<li> <button data-UUID="'+ post.id +'" type="button" id="deleteButton"> Delete</button> <button type="button"  id="updateButton">Update</button> <b>text:</b> ' + post.text + " name: " + post.name + ' at ' + post.created + '</li>');
+				$posts.append('<li> <button data-UUID="'+ post.id +'" type="button" id="deleteButton">X</button>  <b>text:</b> <span class="noEdit text">' + post.text + " </span> <input class='edit text'/> <b>name:</b> <span class='noEdit name'>" + post.name + '</span> <input class="edit name"/> <b>at</b> ' + post.created + ' <button type="button"  class="editPost noEdit">Edit</button> <button type="button" class="saveEdit edit">Save</button><button class="cancelEdit edit">Cancel</button></li>');
 			});
 		},
 		error: function(){
@@ -34,7 +34,7 @@ var $name = $("#name");
 			data: post,
 			success: function(newPost) {
 				console.log(newPost);
-				$posts.append('<li> <button data-UUID="'+ newPost.id +'" type="button" id="deleteButton"> Delete</button> <button type="button" id="updateButton">Update</button> text: ' + newPost.text + " name: " + newPost.userName + '  at ' + Date(newPost.created) + ' </li>');
+				$posts.append('<li> <button data-UUID="'+ post.id +'" type="button" id="deleteButton">X</button>  <b>text:</b> <span class="noEdit text">' + newPost.text + " </span> <input class='edit text'/> <b>name:</b> <span class='noEdit name'>" + newPost.name + '</span> <input class="edit name"/> <b>at</b> ' + newPost.created + ' <button type="button"  class="editPost noEdit">Edit</button> <button type="button" class="saveEdit edit">Save</button><button class="cancelEdit edit">Cancel</button></li>');
 			},
 			error: function(){
 				alert('Couldn\'t load previous posts!');
@@ -51,14 +51,52 @@ var $name = $("#name");
 			success: function(posts) {
 				console.log(posts);
 				$posts.html(""); //this clears page
-				
 				$.each(posts, function(index, post){
-					$posts.append('<li> <button data-UUID="'+ post.id +'" type="button" id="deleteButton"> Delete</button> <button type="button"  id="updateButton">Update</button> <b>text:</b> ' + post.text + " name: " + post.name + ' at ' + post.created + '</li>');
+					$posts.append('<li> <button data-UUID="'+ post.id +'" type="button" id="deleteButton">X</button>  <b>text:</b> <span class="noEdit text">' + post.text + " </span> <input class='edit text'/> <b>name:</b> <span class='noEdit name'>" + post.name + '</span> <input class="edit"/> <b>at</b> ' + post.created + ' <button type="button"  class="editPost noEdit">Edit</button> <button type="button" class="saveEdit edit">Save</button><button class="cancelEdit edit">Cancel</button></li>');
 				});
+			},
+error: function(){
+				alert('error deleting');
 			}
 		});
 
 	});
+
+	$posts.delegate('.editPost', 'click', function() {
+		var $li = $(this).closest('li');
+		$li.find('input.text').val($li.find('span.text').html() );
+		$li.find('input.name').val($li.find('span.name').html() );
+		$li.addClass('edit');
+	});
+
+	$posts.delegate('.cancelEdit', 'click', function() {
+		$(this).closest('li').removeClass('edit');
+	});
+
+	$posts.delegate('.saveEdit', 'click', function() {
+		var $li= $(this).closest('li');
+		var post = {
+			text: $li.find('input.text').val(),
+			name: $li.find('input.name').val()
+		};
+
+		$.ajax({
+			type:'PUT',
+			url:'/posts/' + $(this).attr('data-UUID'),
+			data: post,
+			success: function(newPost) {
+				console.log(newPost);
+				$posts.append('<li> <button data-UUID="'+ newPost.id +'" type="button" id="deleteButton"> Delete</button> <button type="button" id="updateButton">Update</button> text: ' + newPost.text + " name: " + newPost.userName + '  at ' + Date(newPost.created) + ' </li>');
+				$li.find('span.text').html(newPost.text);
+				$li.find('span.name').html(newPost.name);
+				$li.removeClass('edit');				
+			},
+			error: function(){
+				alert('Couldn\'t load previous posts!');
+			},
+		})		
+	});
+
 });
 
 
