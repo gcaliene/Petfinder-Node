9e130c15552f657819d959f4661969cf70f2d1e5
gerson C <gersoncalienes@gmail.com>
2017-10-26 11:46:39 -0400

    Added more validations. now password needs a minimum length, no white spaces are allowed, and must be strings

--- routes/users.js
+++ routes/users.js
@@ -71,6 +71,69 @@ function ensureAuthenticated(req,res,next){
 router.post("/register", function(req, res){
 	//console.log('?????????????')
 	//console.log(req);
+	const stringFields = ['username', 'password'];
+	const nonStringField = stringFields.find(field =>
+	  (field in req.body) && typeof req.body[field] !== 'string'
+	);
+	if (nonStringField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Incorrect field type: expected string',
+            location: nonStringField
+        });
+	}
+	
+	const explicitlyTrimmedFields = ['username', 'password'];
+	const nonTrimmedField = explicitlyTrimmedFields.find(field =>
+	  req.body[field].trim() !== req.body[field]
+	);
+
+	if (nonTrimmedField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Cannot start or end with whitespace',
+            location: nonTrimmedField
+        });
+    }
+
+    const sizedFields = {
+        username: {
+            min: 1
+        },
+        password: {
+            min: 7,
+            // bcrypt truncates after 72 characters, so let's not give the illusion
+            // of security by storing extra (unused) info
+            max: 72
+        }
+    };
+    const tooSmallField = Object.keys(sizedFields).find(
+        field =>
+            'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+    );
+    const tooLargeField = Object.keys(sizedFields).find(
+        field =>
+            'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+    );
+
+    if (tooSmallField || tooLargeField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: tooSmallField
+                ? `Must be at least ${sizedFields[tooSmallField]
+                      .min} characters long`
+                : `Must be at most ${sizedFields[tooLargeField]
+                      .max} characters long`,
+            location: tooSmallField || tooLargeField
+        });
+    }
+
+
 	var name = req.body.name;
 	console.log(req.body);
 	var email = req.body.email;
@@ -120,6 +183,7 @@ router.post("/register", function(req, res){
 			// Forward validation errors on to the client, otherwise give a 500
 			// error because something unexpected has happened
 			if (err.reason === 'ValidationError') {
+				
 				return res.status(err.code).json(err);
 			}
 			res.status(500).json({code: 500, message: 'Internal server error'});
