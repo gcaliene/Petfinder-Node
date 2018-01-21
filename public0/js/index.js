ed4ae72ec3f7e67d6a5fe5d799fbbda9dac7f5ba
Windows <gersoncalienes@gmail.com>
2018-01-21 01:22:19 -0500

    Added error messages to register

--- public0/js/index.js
+++ public0/js/index.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
   const token = localStorage.getItem('token');
-  if(token !== null) {
+  if (token !== null) {
     // window.location.replace("/app.html");
-    $('#nav-registerlogin').addClass('hidden')
-    $('#registration-login').addClass('hidden')
-    $('#header-register-button').addClass('hidden')
-    $('#header-login-button').addClass('hidden')
+    $('#nav-registerlogin').addClass('hidden');
+    $('#registration-login').addClass('hidden');
+    $('#header-register-button').addClass('hidden');
+    $('#header-login-button').addClass('hidden');
   } else {
-    $('#nav-logout').addClass('hidden')
+    $('#nav-logout').addClass('hidden');
   }
 
-
-  $("#nav-logout").on("click" , function(){
-    localStorage.removeItem('token')
-    window.location = '/'
-  })
-
+  $('#nav-logout').on('click', function() {
+    localStorage.removeItem('token');
+    window.location = '/';
+  });
 
   $('#register').on('click', function(event, username, password) {
     var username = $('#username').val();
@@ -29,19 +27,49 @@ $(document).ready(function() {
       contentType: 'application/json',
       data: json,
       success: function(data) {
-        console.log(data);
-        window.location = 'index.html';
-        alert(`You are registered with ${data.username}, please login.`);
+        // console.log(data);
+        $('#error-username-login').html('');
+        $('#error-password-registration').html('');
+        $('#error-username-registration').html('');
+        $('#username').val('');
+        $('#password').val('');
+        $('#error-username-login').removeClass(
+          'class-error-username-registration'
+        );
+        $('#error-username-login').addClass('class-success-username-login');
+        $('#success-username-login').removeClass('hidden');
+        $('#success-username-login').append(
+          `Registration Success! Please Login Below.`
+        );
       },
       error: function(error) {
-        //console.log({ error });
-
-        alert(
-          `${error.status} error at ${error.responseJSON.location}: "${
-            error.responseJSON.message
-          }".`
-        );
+        if (error.responseJSON.location === 'password') {
+          $('#error-username-login').html('');
+          $('#error-password-registration').html('');
+          $('#error-username-registration').html('');
+          $('#error-password-registration').removeClass('hidden');
+          $('#error-password-registration').append(
+            `${error.responseJSON.location.toUpperCase()}: ${
+              error.responseJSON.message
+            }`
+          );
+        } else {
+          $('#error-username-login').html('');
+          $('#error-password-registration').html('');
+          $('#error-username-registration').html('');
+          $('#error-username-registration').removeClass('hidden');
+          $('#error-username-registration').append(
+            `${error.responseJSON.location.toUpperCase()}: ${
+              error.responseJSON.message
+            }`
+          );
+        }
       }
+      // alert(
+      //   `${error.status} error at ${error.responseJSON.location}: "${
+      //     error.responseJSON.message
+      //   }".`
+      // );
     });
   });
 
