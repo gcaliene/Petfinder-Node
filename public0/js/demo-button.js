bb09b6d75fb94952547e204840385635e4801699
Windows <gersoncalienes@gmail.com>
2018-01-22 20:30:04 -0500

    UPdated buttons and added a demo user

---
+++ public0/js/demo-button.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#js-demo-button').on('click', function() {
+    $('#js-demo-button').addClass('hidden');
+    $('#js-demo-user-container').removeClass('hidden');
+  });
+});
